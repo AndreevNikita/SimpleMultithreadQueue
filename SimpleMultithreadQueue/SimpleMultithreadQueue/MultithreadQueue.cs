3737e4ee137ccfd36a7b8f8ac82c4b4010edba18
Nikita Andreev <nickandreevart@gmail.com>
2021-01-04 01:59:57 +0300

    Fixed namespace name

--- SimpleMultithreadQueue/SimpleMultithreadQueue/MultithreadQueue.cs
+++ SimpleMultithreadQueue/SimpleMultithreadQueue/MultithreadQueue.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SimpleMutithreadQueue {
+namespace SimpleMultithreadQueue {
 
 	/*
 	 * Добавляет элементы много потоков, принимает один
